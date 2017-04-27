fc9a3cc153b5b9d27de8a82a96e56ec931004135
ordagan <theorchoo@gmail.com>
2017-04-27 19:56:44 +0300

    commit

--- app/src/main/java/com/notifismart/silencio/phoneService.java
+++ app/src/main/java/com/notifismart/silencio/phoneService.java
@@ -8,14 +8,29 @@ import android.telephony.PhoneStateListener;
 import android.telephony.TelephonyManager;
 import android.util.Log;
 
+import java.lang.reflect.Method;
+
 public class phoneService extends BroadcastReceiver {
 
     @Override
     public void onReceive(Context context, Intent intent) {
-        //Create object of Telephony Manager class.
-        TelephonyManager telephony = (TelephonyManager) context.getSystemService(Context.TELEPHONY_SERVICE);
-        //Assign a phone state listener.
-        CustomPhoneStateListener customPhoneListener = new CustomPhoneStateListener (context);
-        telephony.listen(customPhoneListener, PhoneStateListener.LISTEN_CALL_STATE);
+        TelephonyManager tm = (TelephonyManager) context.getSystemService(Context.TELEPHONY_SERVICE);
+        try {
+            Class c = Class.forName(tm.getClass().getName());
+            Method m = c.getDeclaredMethod("getITelephony");
+            m.setAccessible(true);
+            telephonyService = (ITelephony) m.invoke(tm);
+            Bundle bundle = intent.getExtras();
+            String phoneNumber = bundle.getString("incoming_number");
+            Log.d("INCOMING", phoneNumber);
+            if ((phoneNumber != null)) {
+                telephonyService.endCall();
+                Log.d("HANG UP", phoneNumber);
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
     }
 }
